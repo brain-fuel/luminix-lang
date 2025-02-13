27605ed1a24fd5154170728b7486382410096140
brain-fuel <matt@brain-fuel.co>
2025-02-12 19:45:48 -0500

    add tests for failure to parse true, false, and some paren cases

--- parser/boolean/boolean_test.go
+++ parser/boolean/boolean_test.go
@@ -1,10 +1,28 @@
 package boolean
 
 import (
+	"errors"
+	"fmt"
+	"strings"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
 )
+
+func TestTrueFail(t *testing.T) {
+	tests := []string{
+		"t rue",
+		"tr ue",
+		"tru e",
+		"ture",
+	}
+	for _, test := range tests {
+		_, err := LXBoolParser.ParseString("", test)
+		errorStr := fmt.Sprintf("1:1: unexpected token \"%s\"", strings.Fields(test)[0])
+		expectedErr := errors.New(errorStr)
+		assert.EqualError(t, err, expectedErr.Error())
+	}
+}
 
 func TestTrue(t *testing.T) {
 	input := "true"
@@ -14,12 +32,104 @@ func TestTrue(t *testing.T) {
 	assert.Equal(t, expected, result.LXBool.True)
 }
 
+func TestTrueFailAfterTrueSucceed(t *testing.T) {
+	tests := []string{
+		"true t rue",
+		"true tr ue",
+		"true tru e",
+		"true ture",
+	}
+	for _, test := range tests {
+		_, err := LXBoolParser.ParseString("", test)
+		errorStr := fmt.Sprintf("1:6: unexpected token \"%s\"", strings.Fields(test)[1])
+		expectedErr := errors.New(errorStr)
+		assert.EqualError(t, err, expectedErr.Error())
+	}
+}
+
+func TestTrueFailAfterTrueSucceedAndNewLine(t *testing.T) {
+	tests := []string{
+		"true\nt rue",
+		"true\ntr ue",
+		"true\ntru e",
+		"true\nture",
+	}
+	for _, test := range tests {
+		_, err := LXBoolParser.ParseString("", test)
+		errorStr := fmt.Sprintf("2:1: unexpected token \"%s\"", strings.Fields(test)[1])
+		expectedErr := errors.New(errorStr)
+		assert.EqualError(t, err, expectedErr.Error())
+	}
+}
+
+func TestFalseFail(t *testing.T) {
+	tests := []string{
+		"f alse",
+		"fa lse",
+		"fal se",
+		"fals e",
+		"flase",
+	}
+	for _, test := range tests {
+		_, err := LXBoolParser.ParseString("", test)
+		errorStr := fmt.Sprintf("1:1: unexpected token \"%s\"", strings.Fields(test)[0])
+		expectedErr := errors.New(errorStr)
+		assert.EqualError(t, err, expectedErr.Error())
+	}
+}
+
 func TestFalse(t *testing.T) {
 	input := "false"
 	expected := Ptr(FALSE)
 	result, err := LXBoolParser.ParseString("", input)
 	assert.NoError(t, err)
 	assert.Equal(t, expected, result.LXBool.False)
+}
+
+func TestFalseFailAfterFalseSucceed(t *testing.T) {
+	tests := []string{
+		"false f alse",
+		"false fa lse",
+		"false fal se",
+		"false fals e",
+		"false flase",
+	}
+	for _, test := range tests {
+		_, err := LXBoolParser.ParseString("", test)
+		errorStr := fmt.Sprintf("1:7: unexpected token \"%s\"", strings.Fields(test)[1])
+		expectedErr := errors.New(errorStr)
+		assert.EqualError(t, err, expectedErr.Error())
+	}
+}
+
+func TestFalseFailAfterFalseSucceedAndNewLine(t *testing.T) {
+	tests := []string{
+		"false\nf alse",
+		"false\nfa lse",
+		"false\nfal se",
+		"false\nfals e",
+		"false\nflase",
+	}
+	for _, test := range tests {
+		_, err := LXBoolParser.ParseString("", test)
+		errorStr := fmt.Sprintf("2:1: unexpected token \"%s\"", strings.Fields(test)[1])
+		expectedErr := errors.New(errorStr)
+		assert.EqualError(t, err, expectedErr.Error())
+	}
+}
+
+func TestParensFailWithSingleTrue(t *testing.T) {
+	tests := []string{
+		"(true ",
+		" (true",
+		"( true",
+	}
+	for _, test := range tests {
+		_, err := LXBoolParser.ParseString("", test)
+		errorStr := "1:7: unexpected token \"<EOF>\" (expected \")\")"
+		expectedErr := errors.New(errorStr)
+		assert.EqualError(t, err, expectedErr.Error())
+	}
 }
 
 func TestParens(t *testing.T) {
